363c48528b641c59eed9f9a711349bb50e465386
Jaeho <benteke@naver.com>
2022-01-25 15:02:57 +0900

    Add:Main 화면 구현

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cal1 from '../src/pages/Cal1';
 import Cal2 from '../src/pages/Cal2';
+import Main from './Component/main';
 
 function Router() {
   return (
@@ -8,6 +9,7 @@ function Router() {
       <Routes>
         <Route path="/cal1" element={<Cal1 />} />
         <Route path="/Cal2" element={<Cal2 />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
