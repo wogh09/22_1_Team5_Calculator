2064c80f9ae206cf4c0a0365b4527fb8470be88a
Jaeho <benteke@naver.com>
2022-01-25 22:59:13 +0900

    Add:axios 추가 , useState 상태관리 추가 , priceToString 함수 추가

--- src/Component/main.js
+++ src/Component/main.js
@@ -5,10 +5,10 @@ export default function Main() {
     <div className="flex justify-center h-screen items-center">
       <div className=" w-100 ">
         <div className="text-xs font-bold mb-20">응급코딩과</div>
-        <Link to="/Cal1">
+        <Link to="/cal1">
           <div className="text-xs font-medium mb-40 ">1번 환율계산기!</div>
         </Link>
-        <Link to="/Cal2">
+        <Link to="/cal2">
           <div className="text-xs font-medium ">2번 환율계산기!</div>
         </Link>
       </div>
