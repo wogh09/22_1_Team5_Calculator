5ba905adebdc378e6f84eede878bd0c882d20411
dnjstd <dnjstd@gmail.com>
2022-01-25 10:25:44 +0900

    Add: 레이아웃 구현

---
+++ src/pages/Cal2/currencyData.js
@@ -0,0 +1,22 @@
+export const CURRENCY_LIST = [
+  {
+    id: 1,
+    value: 'CAD',
+  },
+  {
+    id: 2,
+    value: 'KRW',
+  },
+  {
+    id: 3,
+    value: 'HKD',
+  },
+  {
+    id: 4,
+    value: 'JPY',
+  },
+  {
+    id: 5,
+    value: 'CNY',
+  },
+];
